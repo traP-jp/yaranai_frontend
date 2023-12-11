5928b81a17f3f853d276113a84dfe725fb70c8f4
Pugma <maronmarondelicious@gmail.com>
2023-12-12 05:50:32 +0900

    tsファイル自動生成のライブラリ変更 / これでtsの自動型生成まで到達

---
+++ src/apis/generated/AppClient.ts
@@ -0,0 +1,38 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { BaseHttpRequest } from './core/BaseHttpRequest';
+import type { OpenAPIConfig } from './core/OpenAPI';
+import { FetchHttpRequest } from './core/FetchHttpRequest';
+
+import { SuggestionService } from './services/SuggestionService';
+import { TaskService } from './services/TaskService';
+
+type HttpRequestConstructor = new (config: OpenAPIConfig) => BaseHttpRequest;
+
+export class AppClient {
+
+    public readonly suggestion: SuggestionService;
+    public readonly task: TaskService;
+
+    public readonly request: BaseHttpRequest;
+
+    constructor(config?: Partial<OpenAPIConfig>, HttpRequest: HttpRequestConstructor = FetchHttpRequest) {
+        this.request = new HttpRequest({
+            BASE: config?.BASE ?? 'https://yaranai.trap.show/api',
+            VERSION: config?.VERSION ?? '0.0.2',
+            WITH_CREDENTIALS: config?.WITH_CREDENTIALS ?? false,
+            CREDENTIALS: config?.CREDENTIALS ?? 'include',
+            TOKEN: config?.TOKEN,
+            USERNAME: config?.USERNAME,
+            PASSWORD: config?.PASSWORD,
+            HEADERS: config?.HEADERS,
+            ENCODE_PATH: config?.ENCODE_PATH,
+        });
+
+        this.suggestion = new SuggestionService(this.request);
+        this.task = new TaskService(this.request);
+    }
+}
+
