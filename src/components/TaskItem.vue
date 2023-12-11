af06592c0958ead7fff6e7f2442bcc5c96ace1b8
Pugma <maronmarondelicious@gmail.com>
2023-12-11 13:23:14 +0900

    タスクコンポーネント実装 / それの試験的配置 / アニメーション削除 / サイト名の反映

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{
+  name: string
+  Id: string
+  description: string
+}>()
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      {{ props.name }}
+    </div>
+    <div>
+      {{ props.description }}
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 100%;
+  width: 30vw;
+  height: auto;
+  border: 5px;
+  box-shadow: 5px;
+  background-color: white;
+  border-radius: 10px;
+  padding: auto;
+  padding-left: 20px;
+}
+</style>
