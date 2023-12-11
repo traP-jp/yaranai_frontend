b3570e4b3d42deda161d32392fce63be61381c3b
Pugma <maronmarondelicious@gmail.com>
2023-12-11 20:40:32 +0900

    これでPWAの実装が少し進んだ？

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,26 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: 'yaranai',
+        short_name: 'yaranai',
+        description: '新感覚タスク管理アプリ',
+        icons: [], // あとで追加します
+        start_url: 'index.html',
+        display: 'standalone', // デフォルトなので不要
+        background_color: '#BD913F',
+        theme_color: '#916028',
+        lang: 'ja'
+      },
+      includeAssets: [],
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
