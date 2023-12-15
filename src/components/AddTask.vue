ac49274e66541804dada7e13b55ab978225dbf4e
Pugma <maronmarondelicious@gmail.com>
2023-12-15 10:50:58 +0900

    APIつなぎ込みの部分だけ先行実装

---
+++ src/components/AddTask.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import apiClient from '@/apis'
+import type { taskWithoutId } from '@/apis/generated'
+import { ref } from 'vue'
+
+const newTask = ref<taskWithoutId>({
+  title: '',
+  description: '',
+  condition: 0,
+  difficulty: 0,
+  dueDate: ''
+})
+
+const sendNewTask = () => {
+  apiClient.task.postTasks(newTask.value)
+}
+</script>
+
+<template>
+  <div>
+    <h2>新しいタスクを追加する</h2>
+    <v-btn @click="sendNewTask">送信する</v-btn>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
