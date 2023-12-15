53ab7cafc9c7234a305aa2edb275cdde64020999
Pugma <maronmarondelicious@gmail.com>
2023-12-15 11:09:04 +0900

    divの追加による切り分け / こなせる状況取得のAPIの追加実装

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import apiClient from '@/apis'
-import type { taskWithoutId } from '@/apis/generated'
+import type { conditions, taskWithoutId } from '@/apis/generated'
 import { ref } from 'vue'
 
 const newTask = ref<taskWithoutId>({
@@ -11,6 +11,10 @@ const newTask = ref<taskWithoutId>({
   dueDate: ''
 })
 
+const conditionList = ref<conditions>([])
+
+apiClient.condition.getConditions().then((res) => (conditionList.value = res))
+
 const sendNewTask = () => {
   apiClient.task.postTasks(newTask.value)
 }
@@ -19,6 +23,22 @@ const sendNewTask = () => {
 <template>
   <div>
     <h2>新しいタスクを追加する</h2>
+    <div>
+      <p>タスク名</p>
+    </div>
+    <div>
+      <p>概要</p>
+    </div>
+    <div>
+      <p>ハードル</p>
+    </div>
+    <div>
+      <p>状況</p>
+      <span v-for="item in conditionList" :key="item.id"> <v-btn />{{ item.name }} </span>
+    </div>
+    <div>
+      <p>締め切り</p>
+    </div>
     <v-btn @click="sendNewTask">送信する</v-btn>
   </div>
 </template>
