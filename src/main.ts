6db426c056aaccb640d4e4047343497e56937127
Pugma <maronmarondelicious@gmail.com>
2023-12-16 12:31:07 +0900

    自動更新の手順の実装？をした

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,12 @@ import { useRegisterSW } from 'virtual:pwa-register/vue'
 
 const intervalMS = 60 * 60 * 1000
 
-const updateSW = useRegisterSW({
+const updateServiceWorker = useRegisterSW({
+  onRegistered(r) {
+    r && setInterval(()=>{
+      r.update()
+    },intervalMS)     
+  },
   onOfflineReady() {}
 })
 
