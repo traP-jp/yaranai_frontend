013600e31213212b6d7114a43ab7cdc05464faac
Pugma <maronmarondelicious@gmail.com>
2023-12-17 09:07:34 +0900

    strategies:'injectManifest'を選択した

--- vite.config.ts
+++ vite.config.ts
@@ -9,23 +9,45 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
+      base: '/',
+      strategies:'injectManifest',
       registerType: 'autoUpdate',
+      injectRegister:null,
       manifest: {
         name: 'yaranai',
         short_name: 'yaranai',
         description: '新感覚タスク管理アプリ',
-        icons: [], // あとで追加します
+        icons: [
+          {
+            src: '/favicon.ico',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ], // あとで追加します
         start_url: 'index.html',
         display: 'standalone', // デフォルトなので不要
         background_color: '#ffffff',
         theme_color: '#ffffff',
         lang: 'ja'
       },
-      workbox: { cleanupOutdatedCaches: true, globPatterns: ['**/*.{js,css,html,ico,png,svg}'] },
-      includeAssets: ["favicon.ico, 'logo.svg"],
+      workbox: {
+        cleanupOutdatedCaches: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            handler: 'NetworkOnly',
+            urlPattern: /\/api/,
+            method: 'POST',
+            options: { backgroundSync: { name: 'lll', options: { maxRetentionTime: 24 * 60 } } }
+          }
+        ]
+      },
+      includeAssets: ['favicon.ico', 'logo.svg'],
       devOptions: {
-        enabled: false
-      }
+        enabled: true
+      },
+      srcDir: 'src',
+      filename: 'sw.ts'
     })
   ],
   resolve: {
