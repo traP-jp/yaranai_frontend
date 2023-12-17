e75fd95593758ca6dcc5559f0d79d29830c12fa3
Pugma <maronmarondelicious@gmail.com>
2023-12-17 14:30:41 +0900

    状況追加画面での戻るボタンの配置の変更

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -39,40 +39,42 @@ const format = () => {
 </script>
 
 <template>
-  <router-link :to="{ name: 'TaskDashboard' }">戻る</router-link>
-  <v-card width="60vw">
-    <v-card-title>新しいタスクを追加する</v-card-title>
-    <div>
-      <v-text-field
-        label="タスク名"
-        placeholder="タスク名をここに入力"
-        v-model="newTask.title"
-      ></v-text-field>
-    </div>
-    <div>
-      <v-text-field
-        label="概要"
-        placeholder="概要をここに入力"
-        v-model="newTask.description"
-      ></v-text-field>
-    </div>
-    <v-radio-group type="a" label="ハードルの設定" v-model="newTask.difficulty">
-      <span v-for="item in difficultylist" :key="item.id">
-        <v-radio :label="`${item.name}`" color="black" :multiple="false" :value="item.id" />
-      </span>
-    </v-radio-group>
-    <div>
-      <v-radio-group type="a" label="状況" v-model="newTask.condition">
-        <span v-for="item in conditionList" :key="item.id">
+  <div>
+    <router-link :to="{ name: 'TaskDashboard' }">戻る</router-link>
+    <v-card width="60vw">
+      <v-card-title>新しいタスクを追加する</v-card-title>
+      <div>
+        <v-text-field
+          label="タスク名"
+          placeholder="タスク名をここに入力"
+          v-model="newTask.title"
+        ></v-text-field>
+      </div>
+      <div>
+        <v-text-field
+          label="概要"
+          placeholder="概要をここに入力"
+          v-model="newTask.description"
+        ></v-text-field>
+      </div>
+      <v-radio-group type="a" label="ハードルの設定" v-model="newTask.difficulty">
+        <span v-for="item in difficultylist" :key="item.id">
           <v-radio :label="`${item.name}`" color="black" :multiple="false" :value="item.id" />
         </span>
       </v-radio-group>
-    </div>
-    <div>
-      <v-date-picker v-model="date" @update:model-value="format" />
-    </div>
-    <v-btn @click="sendNewTask">送信する</v-btn>
-  </v-card>
+      <div>
+        <v-radio-group type="a" label="状況" v-model="newTask.condition">
+          <span v-for="item in conditionList" :key="item.id">
+            <v-radio :label="`${item.name}`" color="black" :multiple="false" :value="item.id" />
+          </span>
+        </v-radio-group>
+      </div>
+      <div>
+        <v-date-picker v-model="date" @update:model-value="format" />
+      </div>
+      <v-btn @click="sendNewTask">送信する</v-btn>
+    </v-card>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
