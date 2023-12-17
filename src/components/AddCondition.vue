5641f5878811d450447b3114e1cb95288c9a26eb
Pugma <maronmarondelicious@gmail.com>
2023-12-17 12:07:55 +0900

    condition周りの仕様を拡充

--- src/components/AddCondition.vue
+++ src/components/AddCondition.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import apiClient from '@/apis'
+import router from '@/router';
 import { ref } from 'vue'
 
 const conditionReq = ref<{ name: string }>({ name: '' })
@@ -7,20 +8,29 @@ const conditionReq = ref<{ name: string }>({ name: '' })
 const sendNewCondition = () => {
   apiClient.condition.postCondition(conditionReq.value)
 }
+
+const sendBackToDashboard = () => {
+  sendNewCondition()
+  router.push('/')
+}
+const sendContinuingPost = () => {
+  sendNewCondition()
+  conditionReq.value.name = ''
+}
 </script>
 
 <template>
   <v-card width="60vw">
-    <v-card-title>新しいタスクを追加する</v-card-title>
+    <v-card-title>新しい状況を追加する</v-card-title>
     <div>
-      {{ conditionReq.name }}
       <v-text-field
         label="状況名"
         placeholder="タスクのこなせる状況名をここに入力"
         v-model="conditionReq.name"
       />
     </div>
-    <v-btn @click="sendNewCondition">送信する</v-btn>
+    <v-btn @click="sendBackToDashboard">送信してダッシュボードにもどる</v-btn>
+    <v-btn @click="sendContinuingPost">送信し、このまま登録を続ける</v-btn>
   </v-card>
 </template>
 
